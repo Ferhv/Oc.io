6dda55199e8f7a0e458c2b68197bc3291a58e9ed
Ferhv <fernandohv2000@gmail.com>
2023-05-17 22:02:02 +0200

    cambios  Endpoint

--- servidor/lib/AuthStrategy2.js
+++ servidor/lib/AuthStrategy2.js
@@ -1,25 +1,21 @@
 import passport from 'passport'
 import {Strategy as LocalStrategy} from 'passport-local' // Importamos la estrategia passport-local con el nombre "LocalStrategy"
-import Cliente from '../models/cliente.model.js'
 import Empresa from '../models/empresa.model.js'
 
-// Passport.use() aplica el middleware pasado por parámetros a la instancia de Passport
-// (algo parecido a lo que hacemos con Express.use() para definir middlewares de Express)
-
-export const strategyInit = passport => {
-  passport.use('localCliente',
+export const strategyInit2 = passport => {
+  passport.use('localEmpresa',
   new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
   },
   // Lógica a la que llegará Passport cuando haga el authenticate (login)
   (email, password, done) => {
-    Cliente.query().findOne({email: email}).then(user => {
-        if (!user) return done(null, false, {err: 'Usuario desconocido'}); // Si el usuario no existe, devolver callback con error
+    Empresa.query().findOne({email: email}).then(user => {
+        if (!user) return done(null, false, {err: 'Empresa desconocida'}); // Si la empresa no existe, devolver callback con error
         user.verifyPassword(String(password), ((err, passwordIsCorrect) => { // Si existe, validar contraseña
           if (!!err) return done(err); // Si la validación contiene error, devolver callback con error
           if (!passwordIsCorrect) return done(null, false); // Si la contraseña no es válida, devolver callback sin usuario
-          return done(null, user); // Si la contraseña es válida, devolver callback con usuario
+          return done(null, user); // Si la contraseña es válida, devolver callback con empresa
         }))
       }).catch(function (err) {
         done(err) // Si se captura alguna excepción no controlada, devolver callback con error
@@ -35,11 +31,10 @@ passport.serializeUser((user, done) => {
 
 // Deserializar usuarios
 passport.deserializeUser((email, done) => {
-  Cliente.query().findById(email).then((user) => {
+  Empresa.query().findById(email).then((user) => {
     done(null, user)
   })
 })
 }
 
-export default strategyInit;
-
+export default strategyInit2;

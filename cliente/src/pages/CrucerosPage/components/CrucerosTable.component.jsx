5a4f0929f0d450b2a217a2a9e2e86d4217e05c29
cardona326 <sara.cardona@alumnos.uva.es>
2023-06-19 20:35:12 +0200

    Cambios front

---
+++ cliente/src/pages/CrucerosPage/components/CrucerosTable.component.jsx
@@ -0,0 +1,36 @@
+import {Box, Collapse, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Paper, IconButton} from '@mui/material';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import axios from 'axios';
+import { useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import * as React from 'react';
+
+
+export const CrucerosTable = () => {
+  const [conciertos, setCruceros] = useState([]);
+  useEffect(() => {
+    axios({
+        url: 'http://localhost:8080/cruceros',
+        method: 'POST',
+    })
+    .then(res => {
+        setCruceros(res.data);
+    })
+    .catch(err => console.log(err))
+}, []);
+
+  <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+  {cruceros.map((item, index) => (
+    <ImageListItem key={item.img}>
+      <img
+        src={`../assets/img/${index}.png`}
+        alt={item.name}
+        loading="lazy"
+      />
+    </ImageListItem>
+  ))}
+</ImageList>
+}
+export default CrucerosTable;

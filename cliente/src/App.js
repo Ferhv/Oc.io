2033bc5c6414b09a93f87e5bfc6bc8e65df158c5
cardona326 <sara.cardona@alumnos.uva.es>
2023-06-20 16:54:41 +0200

    ultimos cambios

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import ConciertosTable from './pages/CrucerosPage/components/CrucerosTable.component';
+import CrucerosTable from './pages/CrucerosPage/components/CrucerosTable.component';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <ConciertosTable/>
+      <CrucerosTable/>
   )
 }
 
